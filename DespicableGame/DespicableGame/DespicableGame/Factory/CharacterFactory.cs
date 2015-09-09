68d7cfb517eb8258058f7d003e9ba85482b7be97
Samuel <samuel.theriault.hall@gmail.com>
2015-09-09 15:13:10 -0400

    Factory folder

---
+++ DespicableGame/DespicableGame/DespicableGame/Factory/CharacterFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DespicableGame.Factory
+{
+    public static class CharacterFactory
+    {
+
+    }
+}

dc0e56ed63822cd6c3b0837b71f6048923e92a09
Guillaume Ouellet <guillaumeouellet55@hotmail.com>
2015-09-14 13:41:22 -0400

    fixed conflicts

--- DespicableGame/DespicableGame/DespicableGame/DespicableGame.cs
+++ DespicableGame/DespicableGame/DespicableGame/DespicableGame.cs
@@ -23,7 +23,9 @@ namespace DespicableGame
         public const int SCREENHEIGHT = 796;
 
         enum GameStates { PAUSED, PLAYING }
-        public enum GameTextures { HORIZONTAL_WALL, VERTICAL_WALL, WARP_ENTRANCE, WARP_EXIT, GOAL, GRU, POLICE_OFFICER, LEVEL_EXIT, TRAP, SPEEDBOOST, POWERUP_IN_STORE, PLAYERTRAP_COLLECTIBLE, BANANA, BANANA_MINION, POWERUP_MINION, NUMBER_OF_TEXTURES }
+
+        public enum GameTextures { HORIZONTAL_WALL, VERTICAL_WALL, WARP_ENTRANCE, WARP_EXIT, GOAL, GRU, POLICE_OFFICER, LEVEL_EXIT, TRAP, SPEEDBOOST, POWERUP_IN_STORE, PLAYERTRAP_COLLECTIBLE, BANANA, BANANA_MINION, ALERTED_POLICE, POWERUP_MINION, NUMBER_OF_TEXTURES }
+
 
         static Texture2D[] gameTextures = new Texture2D[(int)GameTextures.NUMBER_OF_TEXTURES];
 
@@ -118,6 +120,7 @@ namespace DespicableGame
             gameTextures[(int)GameTextures.BANANA] = Content.Load<Texture2D>("Sprites\\Banana");
             gameTextures[(int)GameTextures.BANANA_MINION] = Content.Load<Texture2D>("Sprites\\Minion2");
             gameTextures[(int)GameTextures.POWERUP_MINION] = Content.Load<Texture2D>("Sprites\\Minion1");
+            gameTextures[(int)GameTextures.ALERTED_POLICE] = Content.Load<Texture2D>("Sprites\\AlertedPolice");
             textFont = Content.Load<SpriteFont>("Fonts/gamefont");
 
             gamePad = new Gamepad(GameManager.GetInstance().Gru, this);
